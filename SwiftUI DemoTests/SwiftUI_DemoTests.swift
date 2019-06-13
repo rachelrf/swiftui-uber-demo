791cf63853198a4862f06e3e6006346a2589ba50
Rachel RoseFigura <rachelrf@uber.com>
2019-06-12 20:38:55 -0700

    Initial Commit

---
+++ SwiftUI DemoTests/SwiftUI_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_DemoTests.swift
+//  SwiftUI DemoTests
+//
+//  Created by Rachel RoseFigura on 6/12/19.
+//  Copyright © 2019 Rachel RoseFigura. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_Demo
+
+class SwiftUI_DemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
